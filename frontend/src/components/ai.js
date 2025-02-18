9b1a95ba481922a9ff93de93100df2c889f06175
DÃelia Knoepfli <delia.knoepfli@gmail.com>
2025-02-18 13:00:39 +0100

    first commit for test branch

--- frontend/src/components/ai.js
+++ frontend/src/components/ai.js
@@ -1,6 +1,6 @@
 export async function getRecipeFromChefClaude(list) {
-  const API_BASE_URL = import.meta.env.VITE_BACKEND_URL || ""; // Ensure dynamic backend URL
-
+  // const API_BASE_URL = import.meta.env.VITE_BACKEND_URL || ""; // Ensure dynamic backend URL
+  const API_BASE_URL = import.meta.env.VITE_API_URL || ""; // Ensure dynamic backend URL
   const response = await fetch(`${API_BASE_URL}/`, { // Add /api to route
     method: "POST",
     headers: { "Content-Type": "application/json" },
