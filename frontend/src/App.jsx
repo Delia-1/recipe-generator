8db7407c7623b75315c7d5c0bfd59ebd58378c38
DÃelia Knoepfli <delia.knoepfli@gmail.com>
2025-02-20 20:11:06 +0100

    try prod

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import './style/App.css'
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 
--- frontend/src/components/ai.js
+++ frontend/src/components/ai.js
@@ -1,8 +1,10 @@
 export async function getRecipeFromChefClaude(list) {
   // const API_BASE_URL = import.meta.env.VITE_BACKEND_URL || "";
-  // const API_BASE_URL = import.meta.env.VITE_API_URL || "";
+  const API_BASE_URL = import.meta.env.VITE_API_URL || "";
 
-  const API_BASE_URL = "http://localhost:3000"; // Hardcoded for testing
+
+  // Hardcoded for testing
+  // const API_BASE_URL = "http://localhost:3000";
 
 
   const response = await fetch(`${API_BASE_URL}/`, { // Add /api to route
